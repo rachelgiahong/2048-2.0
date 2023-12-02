bc065fa5ce9b8e78914301776a506b7f679b3ac0
rachelgiahong <rachelgiahong@berkeley.edu>
2023-12-02 13:39:16 -0800

    Initial Commit

---
+++ 2048 2.0/_048_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  _048_2_0App.swift
+//  2048 2.0
+//
+//  Created by Rachel on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct _048_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
